03520ec343fc1ae90a2c92be4ef94c9f5b17cae3
park myung chan <pmcreg35@gmail.com>
2023-07-14 10:16:26 +0900

    Revert "1"
    This reverts commit 820c5b90c1cc89d942d47353c5820dc184fcd508.

--- workspace/Spring/src/main/java/com/pcwk/ehr/aspectj/Member.java
+++
@@ -1,9 +0,0 @@
-package com.pcwk.ehr.aspectj;
-
-public interface Member {
-	
-	int doSave();
-	int doUpdate();
-	int delete();
-	void doRetrieve(int age);
-}
--- workspace/Spring/src/main/java/com/pcwk/ehr/code/dao/CodeDao.java
+++
@@ -1,9 +0,0 @@
-package com.pcwk.ehr.code.dao;
-
-import com.pcwk.ehr.cmn.PcwkLoger;
-import com.pcwk.ehr.cmn.WorkDiv;
-import com.pcwk.ehr.code.domain.CodeVO;
-
-public interface CodeDao extends WorkDiv<CodeVO>, PcwkLoger {
-
-}
