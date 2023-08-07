f40327e10d88f3859fe89ac4f2800f27b291d4b6
yeonju <kjbv0070@gmail.com>
2023-08-07 09:09:34 +0900

    javascript commit

---
+++ workspace/INVISIBLE-HAND-1.0/src/main/webapp/resources/js/payment/payment.js
@@ -0,0 +1,44 @@
+var IMP = window.IMP;
+IMP.init("imp77248336"); // 가맹점 식별코드
+
+var today = new Date();
+var hours = today.getHours(); // 시
+var minutes = today.getMinutes(); // 분
+var seconds = today.getSeconds(); // 초
+var milliseconds = today.getMilliseconds();
+var makeMerchantUid = hours + minutes + seconds + milliseconds;
+
+function requestPay() {
+	IMP.request_pay({
+		pg : 'kakaopay.TC0ONETIME', // 결제할 지점 이름
+		pay_method : 'card', // 결제 방법
+		merchant_uid : "IMP" + makeMerchantUid, // 주문번호
+		name : '구독(1개월)', // 구매할 상품명
+		amount : 100, // 구매할 가격
+		buyer_email : 'Iamport@chai.finance', // 소비자 이메일
+		buyer_name : '아임포트 기술지원팀', // 소비자 이름
+		buyer_tel : '010-1234-5678' // 소비자 전화번호
+	}, function(data) {
+		if (data.success) {
+			var msg = "결제가 완료되었습니다!";
+			msg += '/n주문 번호 : ' + data.merchant_uid;
+			msg += '/n결제 금액 : ' + data.paid_amount;
+			msg += '/n구매자 이름 : ' + data.buyer_name;
+			msg += '/n구매자 이메일 : ' + data.buyer_email;
+			msg += '/n구매자 전화번호 : ' + data.buyer_tel;
+
+			console.log("성공!");
+			/*
+			 * $.ajax({ type : 'post', url : '/paySuccess', data : { "ID" :
+			 * data.buyer_email, "amount" : data.paid_amount }, });
+			 */
+			document.location.href = "/ehr/main/main.do";
+		} else {
+			var msg = data.error_msg;
+
+			alert(msg);
+			location.reload(); // 새로고침
+		}
+		alert(msg);
+	});
+}
