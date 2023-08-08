61e9508df3f0b84e3a26589e2c5bdffbc7055989
dbsghd3820 <dbsghd3820@gmail.com>
2023-08-08 10:38:52 +0900

    데이터를 받아오기 위한 커밋합니다.

--- workspace/Python/SBHIlink.py
+++ workspace/Python/SBHIlink.py
@@ -1,10 +1,9 @@
 import json
-# python 3.x 버전에서 사용 (2.x 버전이라면 from urllib import urlopen)
 from urllib.request import urlopen
 with urlopen(
-        "https://kosis.kr/openapi/statisticsData.do?method=getList&apiKey=MGM3OWUwNGY2ZTgzM2E0MjI3MzliZDliMmUxYTdhMTY=&format=json&jsonVD=Y&userStatsId=dbsghd3820/340/DT_D10111/2/1/20230712152740&prdSe=M&newEstPrdCnt=3") as url:
+        "https://kosis.kr/openapi/statisticsData.do?method=getList&apiKey=MGM3OWUwNGY2ZTgzM2E0MjI3MzliZDliMmUxYTdhMTY=&format=json&jsonVD=Y&userStatsId=dbsghd3820/142/DT_D40141/2/1/20230808102323&prdSe=Y&newEstPrdCnt=3") as url:
     json_file = url.read()
 py_json = json.loads(json_file.decode('utf-8'))
 for temp in py_json:
-    print(temp)
-    #print("경기전반실적:"+temp['DT'] + " " + temp['PRD_DE'])
+    #print(temp)
+    print("[테이블 이름:"+ temp['TBL_NM'] + "], " + "[연도:"+ temp['PRD_DE'] + "], " +  "[자산규모:"+ temp['ITM_NM'] + "], " + "[업종:" + temp['C1_NM'] + "], " + "[입력층:" + temp['C2_NM'] + "], " + "[데이터:" + temp['DT'] + "], ")
