b06ab705390e167de67cbc6ade6e3c96454e2556
dbsghd3820 <dbsghd3820@gmail.com>
2023-08-11 17:38:24 +0900

    예측치 가지고 commit

--- workspace/Python/modeltmp.py
+++ workspace/Python/modeltmp.py
@@ -4,25 +4,36 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import SimpleRNN, Dense
 from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
+import cx_Oracle
 
+def format_datetime(input_datetime):
+    try:
+        formatted_date = input_datetime.strftime('%Y%m')
+        return formatted_date
+    except AttributeError:
+        return "Invalid input. Please provide a valid datetime object."
+
+dsn = cx_Oracle.makedsn('192.168.0.123', 1521, 'xe')
+db = cx_Oracle.connect('INVISIBLEHAND', '4321', dsn)
+cursor = db.cursor()
+
+cursor.execute("select * from sbhitable where subcategory = '건설업' order by chartdate")
+data = cursor.fetchall()
 # 주어진 데이터
-X_data = [
-    [60.5, 70.5, 77.4, 101.4, 69.8, 77.2],
-    [67.2, 76.5, 90.4, 85.7, 92.8, 84.1],
-    [40.5, 20.5, 57.1, 99.4, 89.8, 77.2]
-]
-y_data = [sum(x) for x in X_data]
+x_data = np.array([row[3:9] for row in data])
+y_data = np.array([np.mean(row[3:9]) for row in data])
 
 # 데이터 전처리
-X_data = np.array(X_data)  # 리스트를 넘파이 배열로 변환
+X_data = np.array(x_data)  # 리스트를 넘파이 배열로 변환
 y_data = np.array(y_data)
 
 # 데이터 분할
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.11, random_state=42)
 
 # RNN 모델 구성
 model = Sequential()
-model.add(SimpleRNN(units=32, input_shape=(X_data.shape[1], 1), activation='relu'))
+model.add(SimpleRNN(units=32, input_shape=(X_data.shape[1], 1), activation='relu', return_sequences=True))
+model.add(SimpleRNN(units=16, activation='relu'))
 model.add(Dense(units=1))
 
 # 모델 컴파일
@@ -32,7 +43,7 @@ model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error', me
 model.summary()
 
 # 모델 훈련
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=30, batch_size=32, validation_data=(X_test, y_test))
 
 # 테스트 데이터로 예측
 predictions = model.predict(X_test)
