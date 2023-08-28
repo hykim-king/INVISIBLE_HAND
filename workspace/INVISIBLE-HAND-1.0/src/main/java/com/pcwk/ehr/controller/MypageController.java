bde5d3e8079fd1acb8d65d2a9821aa353929a6b0
yeonju <kjbv0070@gmail.com>
2023-08-28 09:51:04 +0900

    commit

--- workspace/INVISIBLE-HAND-1.0/src/main/java/com/pcwk/ehr/controller/MypageController.java
+++ workspace/INVISIBLE-HAND-1.0/src/main/java/com/pcwk/ehr/controller/MypageController.java
@@ -3,9 +3,6 @@ package com.pcwk.ehr.controller;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,265 +25,181 @@ import com.pcwk.ehr.service.MyPageService;
 import com.pcwk.ehr.service.PaymentInfoService;
 
 @Controller
-@RequestMapping(value = "mypage")	//WEB_INF아래 폴더이름을 적는곳.
+@RequestMapping(value = "mypage") // WEB_INF아래 폴더이름을 적는곳.
 public class MypageController {
-	
+
 	final Logger LOG = LogManager.getLogger(getClass());
-	
+
 	@Autowired
 	PaymentInfoService payService;
-	
+
 	@Autowired
 	MyPageService myPageService;
-	
+
 	@Autowired
 	MemberService memberService;
-	
+
 	@Autowired
 	MailSendService mailSend;
 
-	
-	
 	@RequestMapping(value = "/mypage.do")
 	public String mypage() {
-		LOG.debug("┌───────────────────────┐");
-		LOG.debug("│ mypage()       	   │");
-		LOG.debug("└───────────────────────┘");
-		
+
 		return "mypage/mypage";
 	}
-	
+
 	@RequestMapping(value = "/mypageUpdate.do")
 	public String mypageUpdate() {
-		LOG.debug("┌───────────────────────┐");
-		LOG.debug("│ mypageUpdate()       	   │");
-		LOG.debug("└───────────────────────┘");
-		
+
 		return "mypage/mypageUpdate";
 	}
-	
-	 @RequestMapping(value="/nickcheck.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-	    @ResponseBody
-	    public String nickcheck(MemberVO member) throws Exception {
-	        System.out.println("┌────────────────────────────────────────────────────────┐");
-	        System.out.println("│ nickcheck()                                            │");
-	        System.out.println("└────────────────────────────────────────────────────────┘");
-	        
-	        String jsonString = "";
-	        MessageVO message = new MessageVO();
-	         
-	        int nickcheck = memberService.nickNameCheck(member);
-	        System.out.println("┌─────────────────────────────────" + nickcheck);
-	        
-	        if (nickcheck == 0) {
-	                message.setMsgId("10");
-	                message.setMsgContents("사용 가능한 닉네임 입니다.");
-	        } else {
-	                message.setMsgId("20");
-	                message.setMsgContents("이미 사용중인 닉네임 입니다");
-	        }
-	        
-	        jsonString = new Gson().toJson(message);
-	        return jsonString;
-	    }
-	 
-	 @RequestMapping(value="/passwordcheck.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-	    @ResponseBody
-	    public String passwordcheck(MemberVO member) throws Exception {
-	        System.out.println("┌────────────────────────────────────────────────────────┐");
-	        System.out.println("│ passwordcheck()                                        │");
-	        System.out.println("└────────────────────────────────────────────────────────┘");
-	        
-	        String jsonString = "";
-	        MessageVO message = new MessageVO();
-	         
-	        int passCheck = memberService.passCheck(member);
-	        
-	        if (passCheck == 0) {
-	                message.setMsgId("10");
-	                message.setMsgContents("사용 가능한 비밀번호 입니다.");
-	        } else {
-	                message.setMsgId("20");
-	                message.setMsgContents("이미 사용중인 비밀번호 입니다");
-	        }
-	        
-	        jsonString = new Gson().toJson(message);
-	        return jsonString;
-	    }
-	 
-	 @RequestMapping(value="/emailcheck.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-	    @ResponseBody
-	    public String emailcheck(MemberVO member) throws Exception {
-	        System.out.println("┌────────────────────────────────────────────────────────┐");
-	        System.out.println("│ emailcheck()                                           │");
-	        System.out.println("└────────────────────────────────────────────────────────┘");
-	        
-	        String jsonString = "";
-	        MessageVO message = new MessageVO();
-	         
-	        int emailcheck = memberService.emailCheck(member);
-	        
-	        if (emailcheck == 1) {
-	        	message.setMsgId("10");
-	        	message.setMsgContents("이메일을 전송하시겠습니가 ?");
-	        } else {
-	        	message.setMsgId("20");
-	        	message.setMsgContents("이메일을 전송할 수 없습니다.");
-	        }
-	        
-	        jsonString = new Gson().toJson(message);
-	        return jsonString;
-	    }
-		 
-	 	@RequestMapping(value = "/**/mailSend.do", method = RequestMethod.GET)
-			@ResponseBody
-			public String mailSend(String email) {
-				System.out.println("이메일 인증 요청이 들어옴!");
-				System.out.println("이메일 인증 이메일 :"+email);
-				return mailSend.joinEmail(email);
+
+	@RequestMapping(value = "/nickcheck.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String nickcheck(MemberVO member) throws Exception {
+		String jsonString = "";
+		MessageVO message = new MessageVO();
+
+		int nickcheck = memberService.nickNameCheck(member);
+
+		if (nickcheck == 0) {
+			message.setMsgId("10");
+			message.setMsgContents("사용 가능한 닉네임 입니다.");
+		} else {
+			message.setMsgId("20");
+			message.setMsgContents("이미 사용중인 닉네임 입니다");
 		}
-	 	
-	 	
-	 	@RequestMapping(value="/updatemember.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-		    @ResponseBody
-		    public String updatemember(MemberVO member) throws Exception {
-		        System.out.println("┌────────────────────────────────────────────────────────┐");
-		        System.out.println("│ updatemember()                                               │");
-		        System.out.println("└────────────────────────────────────────────────────────┘");
-		        
-		        String jsonString = "";
-		        MessageVO message = new MessageVO();
-		         
-		        int update = memberService.update(member);
-		        LOG.debug("update===============mem" + update);
-		        if (update == 1) {
-		                message.setMsgId("10");
-		                message.setMsgContents("업데이트 완료");
-		        } else {
-		                message.setMsgId("20");
-		                message.setMsgContents("업데이트 실패");
-		        }
-		        
-		        jsonString = new Gson().toJson(message);
-		        return jsonString;
-		    }
-	
+
+		jsonString = new Gson().toJson(message);
+		return jsonString;
+	}
+
+	@RequestMapping(value = "/passwordcheck.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String passwordcheck(MemberVO member) throws Exception {
+
+		String jsonString = "";
+		MessageVO message = new MessageVO();
+
+		int passCheck = memberService.passCheck(member);
+
+		if (passCheck == 0) {
+			message.setMsgId("10");
+			message.setMsgContents("사용 가능한 비밀번호 입니다.");
+		} else {
+			message.setMsgId("20");
+			message.setMsgContents("이미 사용중인 비밀번호 입니다");
+		}
+
+		jsonString = new Gson().toJson(message);
+		return jsonString;
+	}
+
+	@RequestMapping(value = "/emailcheck.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String emailcheck(MemberVO member) throws Exception {
+
+		String jsonString = "";
+		MessageVO message = new MessageVO();
+
+		int emailcheck = memberService.emailCheck(member);
+
+		if (emailcheck == 1) {
+			message.setMsgId("10");
+			message.setMsgContents("이메일을 전송하시겠습니가 ?");
+		} else {
+			message.setMsgId("20");
+			message.setMsgContents("이메일을 전송할 수 없습니다.");
+		}
+
+		jsonString = new Gson().toJson(message);
+		return jsonString;
+	}
+
+	@RequestMapping(value = "/**/mailSend.do", method = RequestMethod.GET)
+	@ResponseBody
+	public String mailSend(String email) {
+		System.out.println("이메일 인증 요청이 들어옴!");
+		System.out.println("이메일 인증 이메일 :" + email);
+		return mailSend.joinEmail(email);
+	}
+
 	@RequestMapping(value = "/mypageDelete.do", method = RequestMethod.GET)
 	public String mypageDelete() {
 		LOG.debug("┌───────────────────────┐");
 		LOG.debug("│ mypageDelete()       	   │");
 		LOG.debug("└───────────────────────┘");
-		
+
 		return "mypage/mypageDelete";
 	}
-	
-	 @RequestMapping(value="/mypageDelete.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-	    @ResponseBody
-	    public String DeleteOne(MemberVO member) throws SQLException {
-	        System.out.println("┌────────────────────────────────────────────────────────┐");
-	        System.out.println("│ delete()                                            │");
-	        System.out.println("└────────────────────────────────────────────────────────┘");
-	        
-	        String jsonString = "";
-	        MessageVO message = new MessageVO();
-	         
-	        int delete = memberService.deleteOne(member);
-	        
-	        if (delete == 1) {
-	                message.setMsgId("10");
-	                message.setMsgContents("회원 삭제 완료.");
-	        } else {
-	                message.setMsgId("20");
-	                message.setMsgContents("회원 삭제 실패했습니다.");
-	        }
-	        
-	        jsonString = new Gson().toJson(message);
-	        return jsonString;
-	    }
-
 
 	@RequestMapping(value = "/mypagePayedit.do")
 	public String mypagePayedit() {
 		LOG.debug("┌───────────────────────┐");
 		LOG.debug("│ mypagePayedit()       	   │");
 		LOG.debug("└───────────────────────┘");
-		
+
 		return "mypage/mypagePayedit";
 	}
-	
-	
-	@RequestMapping(value = "/mypagePayment.do",method = RequestMethod.GET)
-	public void mypagePayment(Model model,MemberVO member,
-		@RequestParam(value = "buyerEmail", required = false, defaultValue = "") String buyerEmail) throws SQLException{
-		
+
+	@RequestMapping(value = "/mypagePayment.do", method = RequestMethod.GET)
+	public void mypagePayment(Model model, MemberVO member,
+			@RequestParam(value = "buyerEmail", required = false, defaultValue = "") String buyerEmail)
+			throws SQLException {
+
 		List<PaymentInfoVO> paymentList = payService.getPaymentListByEmail(buyerEmail); // 객체에 정보 담기
-		
+
 		model.addAttribute("list", paymentList);
 
 	}
-	
-	@RequestMapping(value = "/mypost.do",method = RequestMethod.GET)
-	public void mypost(Model model,MemberVO member,
-		@RequestParam(value = "nickname",required = false, defaultValue = "") String nickname) throws SQLException{
+
+	@RequestMapping(value = "/mypost.do", method = RequestMethod.GET)
+	public void mypost(Model model, MemberVO member,
+			@RequestParam(value = "nickname", required = false, defaultValue = "") String nickname)
+			throws SQLException {
 		LOG.debug("┌───────────────────────┐");
 		LOG.debug("│ mypost()       	   │");
 		LOG.debug("└───────────────────────┘");
-		
+
 		LOG.debug("┌───────────────────────┐");
-		LOG.debug("│ nickname()       	   ::"+ nickname);
+		LOG.debug("│ nickname()       	   ::" + nickname);
 		LOG.debug("└───────────────────────┘");
 
-		
-		List<PostVO> list = null;
-		
-		list = myPageService.postlist(nickname);
-		
-		model.addAttribute("list",list);
+		List<PostVO> list = myPageService.postlist(nickname);
+
+		model.addAttribute("list", list);
 	}
-	
-	@RequestMapping(value = "/mycomment.do",method = RequestMethod.GET)
-	public void mycomment(Model model,MemberVO member,
-		@RequestParam(value = "nickname",required = false, defaultValue = "") String nickname) throws SQLException{
+
+	@RequestMapping(value = "/mycomment.do", method = RequestMethod.GET)
+	public void mycomment(Model model, MemberVO member,
+			@RequestParam(value = "nickname", required = false, defaultValue = "") String nickname)
+			throws SQLException {
 		LOG.debug("┌───────────────────────┐");
 		LOG.debug("│ mycomment()       	   │");
 		LOG.debug("└───────────────────────┘");
-		
+
 		LOG.debug("┌───────────────────────┐");
-		LOG.debug("│ nickname()       	   ::"+ nickname);
+		LOG.debug("│ nickname()       	   ::" + nickname);
 		LOG.debug("└───────────────────────┘");
 
-		
-		List<CommentVO> list = null;
-		
-		list = myPageService.commentlist(nickname);
-		
-		model.addAttribute("list",list);
-	}
-	
-	@RequestMapping(value = "/myqna.do",method = RequestMethod.GET)
-	public void myqna(Model model,MemberVO member,
-		@RequestParam(value = "nickname",required = false, defaultValue = "") String nickname) throws SQLException{
-		LOG.debug("┌───────────────────────┐");
-		LOG.debug("│ mypost()       	   │");
-		LOG.debug("└───────────────────────┘");
-		
-		LOG.debug("┌───────────────────────┐");
-		LOG.debug("│ nickname()       	   ::"+ nickname);
-		LOG.debug("└───────────────────────┘");
+		List<CommentVO> list = myPageService.commentlist(nickname);
 
-		
-		List<PostVO> list = null;
-		
-		list = myPageService.qnalist(nickname);
-		
-		model.addAttribute("list",list);
+		model.addAttribute("list", list);
 	}
+
+	@RequestMapping(value = "/myqna.do", method = RequestMethod.GET)
+	public void myqna(Model model, MemberVO member,
+			@RequestParam(value = "nickname", required = false, defaultValue = "") String nickname)
+			throws SQLException {
+
+		List<PostVO> list = myPageService.qnalist(nickname);
+
+		model.addAttribute("list", list);
+	}
+
 	public String mypageView() {
-		LOG.debug("┌───────────────────────┐");
-		LOG.debug("│ mypageView()       	   │");
-		LOG.debug("└───────────────────────┘");
-		
+
 		return "mypage/mypageView";
 	}
-	
+
 }
